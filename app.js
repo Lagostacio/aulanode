e330730c6c980804934eddc0361f0ebf80698270
Lagostacio <inaciobortolaslago@gmail.com>
2023-04-12 11:51:58 -0300

    express js

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require("express")
+const bodyParser = require('body-parser')
+
+
+
+const port = process.env.port
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.use('/public', express.static('public'))
+
+
+app.get('/', (_, res) => {
+    res.json({nome: "Inácio"})
+})
+
+app.post('/salvar',(req,res) => {
+    const {nome} = req.body
+    res.json({nome})
+})
+
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`)
+})
